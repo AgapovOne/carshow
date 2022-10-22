ba73d964c9c48eb70d6447c7a45921764a4a1871
Alexey Agapov <agapov.one@gmail.com>
2022-10-22 13:44:57 +0500

    Initial Commit

---
+++ Carshow/CarshowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarshowApp.swift
+//  Carshow
+//
+//  Created by Alexey Agapov on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CarshowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carshow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Carshow
+//
+//  Created by Alexey Agapov on 22.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
